91441de678200d60f922cd5b5b4dda03c0d6d67f
Raymond Nguyen <rayknguyen@gmail.com>
2020-02-07 10:31:26 -0700

    Add test for extension

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -76,6 +76,53 @@ RSpec.describe "on the show page" do
       expect(project_1.average_age).to eq(20)
       expect(page).to have_content("Average age of Contestants #{project_1.average_age}")
     end
+    it "can see a form a to add projects" do
+      ray = Contestant.create(
+        name: "Ray",
+        age: 25,
+        hometown: "Anahiem",
+        years_of_experience: 12)
+      alfredo = Contestant.create(
+        name: "Alfredo",
+        age: 20,
+        hometown: "Denver",
+        years_of_experience: 3)
+      alex = Contestant.create(
+        name: "Alex",
+        age: 15,
+        hometown: "Richmond",
+        years_of_experience: 8)
+
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      project_1 = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      project_2 = recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+      
+      ContestantProject.create(contestant: ray, project: project_1)
+      ContestantProject.create(contestant: alex, project: project_1)
+      ContestantProject.create(contestant: alfredo, project: project_1)
+    
+      visit "/projects/#{project_1.id}"
+
+      expect(page).to have_content("Number of Contestants 3")
+      
+      fill_in :name, with: "Meg"
+      fill_in :material, with: "Snow lots of snow"
+
+      click_on "Add Contestant To Project" 
+
+      expect(current_path).to eq("/projects/#{project_1.id}")
+      expect(page).to have_content("Number of Contestants 4")
+
+      new_contestant = Contestant.last
+
+      visit "/contestants"
+
+      within("#contestant-#{new_contestant.id}") do
+        expect(page).to have_content(new_contestant.name)
+        expect(page).to have_content(project_1.name)
+      end
+    end
   end 
 end
 
